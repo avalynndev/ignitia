69e75ba431f397f363c36d79fa0f5716aede77ac
avalynn <100826194+avalynndev@users.noreply.github.com>
2025-09-07 05:57:46 +0000

    eslint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,8 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'next'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
@@ -19,7 +23,7 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
-  },
+  })
 ];
 
 export default eslintConfig;
